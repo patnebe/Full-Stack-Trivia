b64a6fbc2fbc1b9c3795018c870d013f6a73d3ca
Dev-Nebe <cp.nebe@gmail.com>
2020-03-21 11:22:10 +0100

    chore: Created production build of frontend

---
+++ frontend/build/precache-manifest.d8f810bb9a625d5e527759c3fcb69f3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371d3bea601ee86f17c7d911962616f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "037f318c8118ce647b9b",
+    "url": "/static/css/main.ee25cf69.chunk.css"
+  },
+  {
+    "revision": "c123a49ca2fb96e7d658",
+    "url": "/static/js/2.5fe190b6.chunk.js"
+  },
+  {
+    "revision": "842a247c819a3163f5f5c6bfe6ac8ac9",
+    "url": "/static/js/2.5fe190b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037f318c8118ce647b9b",
+    "url": "/static/js/main.82821eb4.chunk.js"
+  },
+  {
+    "revision": "dcab15498f876814162e",
+    "url": "/static/js/runtime-main.e1faeae6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
